378d38e59ea7ecc176e95ef8b5b2e192e028534c
dlutz2 <dlutz@mitre.org>
2018-04-11 10:16:55 -0400

    Some more abstraction changes

---
+++ src/org/opensextant/howler/abstraction/words/enumerated/PassiveMarker.java
@@ -0,0 +1,84 @@
+package org.opensextant.howler.abstraction.words.enumerated;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.opensextant.howler.abstraction.Vocabulary;
+import org.opensextant.howler.abstraction.Word;
+import org.opensextant.howler.abstraction.phrases.Footnote;
+import org.opensextant.howler.utils.TextUtils;
+import org.semanticweb.owlapi.model.IRI;
+
+public enum PassiveMarker implements Word {
+	BY("by");
+
+	private final String logicalform;
+	private String normalform;
+	private IRI key;
+
+	PassiveMarker(String normalForm) {
+		this.logicalform = TextUtils.createLogicalFromNormal(normalForm);
+		this.normalform = normalForm;
+		this.key = IRI.create(Vocabulary.BUILTIN_NS.toString(), logicalform);
+	}
+
+	public static PassiveMarker getTypeByNormalName(String normal) {
+		return Stream.of(values()).filter(v -> v.getNormalForm().equals(normal)).findAny().orElse(null);
+	}
+
+	public String getLogicalform() {
+		return logicalform;
+	}
+
+	@Override
+	public String getNormalForm() {
+		return normalform;
+	}
+
+	@Override
+	public void setNormalForm(String normalForm) {
+		// ignore
+	}
+
+	@Override
+	public IRI getKey() {
+		return key;
+	}
+
+	@Override
+	public String getLogicalForm() {
+		return logicalform;
+	}
+
+	@Override
+	public String getNamespace() {
+		return Vocabulary.BUILTIN_NS.toString();
+	}
+
+	@Override
+	public Scope getScope() {
+		return Scope.GENERAL;
+	}
+
+	@Override
+	public WordType getWordType() {
+		return WordType.PASSIVE_MARKER;
+	}
+
+	@Override
+	public List<Footnote> getFootnotes() {
+		return new ArrayList<Footnote>();
+	}
+
+	@Override
+	public void setFootnotes(List<Footnote> footnotes) {
+		// ignore
+	}
+
+	@Override
+	public void addFootnote(Footnote footnote) {
+		// ignore
+	}
+
+}
